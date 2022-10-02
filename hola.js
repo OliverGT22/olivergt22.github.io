bf02d520c994066e3586c05230a4633f0edb824b
OliverGT22 <109478445+OliverGT22@users.noreply.github.com>
2022-10-01 18:50:07 -0600

    Update hola.js

--- hola.js
+++ hola.js
@@ -11,24 +11,3 @@ function showPopup()
             alert("El valor ingresado es: "+data); //display variable along with the string
         }
     }
-	
-function estado(){	
-	if(navigator.onLine) {
-		goOnline();
-	} else {
-		goOffile();
-	}
-}
-
-window.addEventListener('offline', goOffline());
-window.addEventListener('online', goOnline());
-
-function goOnline() {
-	document.getElementById("conexion").innerHTML = 'Online';
-	document.getElementById("conexion").style.color = "green";
-}
-
-function goOffline() {
-	document.getElementById("conexion").innerHTML = 'Offline';
-	document.getElementById("conexion").style.color = "red";
-}
